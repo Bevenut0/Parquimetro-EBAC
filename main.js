8739708cef99d6c1b17c26d465efd8e55071da03
victor <victorgabrielbevenuta@gmail.com>
2025-12-31 14:41:39 -0300

    main

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 /* Parquimetro logica 
 
+0- fazer uso de POO
 1- pegar valores de tempo e armazenar
 2- Criar logica de pagamento 
 3- pegar valor enviado pelo usuario 
-
-
-
-
-
-
-
-
+4- realizar troco dependendo do valor
+5- mandar textos para o html
+6- criar opção/ tempo para o site reiniciar
 
 */
